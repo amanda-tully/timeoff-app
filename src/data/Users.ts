de10aa7773112976cdbe7d8866b01fd901532f81
Amanda Tully <amandadianacmd@gmail.com>
2025-10-16 17:01:41 +0200

    Add dummy users file

---
+++ src/data/Users.ts
@@ -0,0 +1,31 @@
+export const users = [
+  {
+    id: "1",
+    name: "Emma",
+    supervisorId: "3",
+    employeesToSupervise: [],
+    availableViews: ["request"],
+  },
+  {
+    id: "2",
+    name: "Bob",
+    supervisorId: "3",
+    employeesToSupervise: [],
+    availableViews: ["request"],
+  },
+  {
+    id: "3",
+    name: "Anna",
+    supervisorId: "4",
+    employeesToSupervise: ["1", "2"],
+    availableViews: ["request", "response"],
+  },
+
+  {
+    id: "4",
+    name: "Frank",
+    supervisorId: null,
+    employeesToSupervise: ["3"],
+    availableViews: ["response"],
+  },
+];
